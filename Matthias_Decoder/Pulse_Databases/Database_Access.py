594c1c528b6fb6f6be1fcd1f1b804928891469a7
GovindKhavish <gvnkha001@myuct.ac.za>
2025-03-06 16:11:39 +0200

    Database access edits

--- Matthias_Decoder/Pulse_Databases/Database_Access.py
+++ Matthias_Decoder/Pulse_Databases/Database_Access.py
@@ -1,6 +1,9 @@
 #=========================================================================================
 # main_pulse_analysis.py
-#
+#Chirp 1 (BW=5.0MHz, FC=-4.0MHz, Duration=10us) injected 123 times
+#Chirp 2 (BW=6.0MHz, FC=-2.0MHz, Duration=15us) injected 115 times
+#Chirp 3 (BW=7.0MHz, FC=2.0MHz, Duration=20us) injected 110 times
+#Chirp 4 (BW=8.0MHz, FC=4.0MHz, Duration=25us) injected 122 times
 #=========================================================================================
 from __future__ import division, print_function, unicode_literals
 
@@ -11,40 +14,41 @@ import Database_Functions  # Custom module for database handling and plotting
 
 #----------------------------------------------------------------------------------------#
 # Path to the database
-#db_folder = r"/Users/khavishgovind/Library/CloudStorage/OneDrive-UniversityofCapeTown/Masters/Databases"
-db_folder = r"C:\Users\govin\UCT_OneDrive\OneDrive - University of Cape Town\Masters\Databases"
-db_name = "pulse_characteristics_Burst_Simulator.db"
+db_folder = r"/Users/khavishgovind/Library/CloudStorage/OneDrive-UniversityofCapeTown/Masters/Databases"
+#db_folder = r"C:\Users\govin\UCT_OneDrive\OneDrive - University of Cape Town\Masters\Databases"
+db_name = "pulse_characteristics_Mipur.db"
+# db_name = "pulse_characteristics_Burst_Simulator.db"
 db_path = f"{db_folder}/{db_name}"
-tolerance = 0.05
+freq_tolerance = 0.08  # 1% tolerance for center frequency
+chirp_tolerance = 0.0147  # 5% tolerance for chirp rate
 
 # Load pulse data
 # pulse_data = Database_Functions.load_pulse_data_from_db(db_path)
-# row_count = Database_Functions.count_rows_in_database(db_path)
-# print(row_count)
-# burst_num = Database_Functions.get_unique_burst_numbers(db_path)
-# print(burst_num)
 
-# # View the database in a window and allow row selection
-Database_Functions.display_database_in_window(db_path)
-#Database_Functions.display_converted_database_in_window(db_path)
-pdw_results = Database_Functions.pdw_analysis(db_path,tolerance)
+# ------------------- Tolerance Analysis ---------------------
+# results = Database_Functions.analyze_tolerance(db_path)
+# print(Database_Functions.print_tolerance_results(results))
+
+# ------------------- Viewing Database ---------------------
+# Database_Functions.display_database_in_window(db_path)
+# #Database_Functions.display_converted_database_in_window(db_path)
+
+# ------------------- Real Data PDW ---------------------
+pdw_results = Database_Functions.pdw_analysis(db_path, freq_tolerance, chirp_tolerance)
 #Database_Functions.plot_pdw_bins(pdw_results, tolerance)
 Database_Functions.plot_pdw_scatter(pdw_results)
-results = Database_Functions.analyze_tolerance(db_path)
-print(Database_Functions.print_tolerance_results(results))
+Database_Functions.display_top_bins(pdw_results)
 #Database_Functions.plot_top_5_pdw_scatter_with_summary_table(pdw_results,tolerance) 
 
 
-# # # Plot the pulse characteristics
-# rows = Database_Functions.count_rows_in_database(db_path)
+# ------------------- Plotting ---------------------
 # Database_Functions.plot_bandwidth_vs_pulse_number(pulse_data)
 # Database_Functions.plot_duration_vs_pulse_number(pulse_data)
 # Database_Functions.plot_center_frequency_vs_pulse_number(pulse_data)
 # Database_Functions.plot_chirp_rate_vs_pulse_number(pulse_data)
 
-#pdw_results = Database_Functions.pdw_analysis(db_path)
 
-# # If I/Q data visualization or specific pulse retrieval is needed
+# ------------------- IQ Data Display ---------------------
 # pulse_number = 2
 # iq_data = Database_Functions.retrieve_iq_data_from_db(pulse_number, db_path)
 # Database_Functions.plot_iq_data(iq_data, pulse_number)
